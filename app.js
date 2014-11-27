19fa9e707426c1de42f09b6d1aa149e59203d8a4
Fabpk90 <fabrizio1996@live.fr>
2014-11-27 19:49:08 +0100

    Added array that contains tags and number of chambers that the Villa has

--- app.js
+++ app.js
@@ -1,12 +1,32 @@
 var app = angular.module('villaSignIn', []);
 
 app.controller('villaSignInCtrl', ['$scope', function ($scope) {
-    $scope.checkData = function () {
-        if ($scope.villaName.length > 5 && $scope.villaOwner.length > 5) {
+    $scope.tag = [];
+
+    $scope.deleteTag = function(item) {
+        var index = $scope.tag.indexOf(item);
+        $scope.tag.splice(index, 1);
+    };
+
+
+    $scope.addTag = function()
+    {
+        if($scope.villaTag.length > 3)
+        {
+            $scope.tag.push({name: $scope.villaTag});
+        }
+
+    };
+
+    $scope.checkData = function ()
+    {
+        if ( ($scope.villaName.length > 5 && $scope.villaOwner.length > 5) && $scope.villaNumber > 0)
+        {
             console.log("ok");
         }
-        else {
-            alert("Fill all the gaps (Min. 5 letters)!");
+        else
+        {
+            alert("Fill all the gaps !");
         }
 
     };
