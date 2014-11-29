4a9a131be26f8f1a1b825f5f1acba04fa454c67f
Fabpk90 <fabrizio1996@live.fr>
2014-11-29 18:38:10 +0100

    Added new lib for drag and drop upload

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var app = angular.module('villaSignIn', []);
+var app = angular.module('villaSignIn', ['angularFileUpload']);
 
 app.controller('villaSignInCtrl', ['$scope', function ($scope) {
     $scope.tag = [];
@@ -20,7 +20,7 @@ app.controller('villaSignInCtrl', ['$scope', function ($scope) {
 
     $scope.checkData = function ()
     {
-        if ( ($scope.villaName.length > 5 && $scope.villaOwner.length > 5) && $scope.villaNumber > 0)
+        if ( ($scope.villaName.length > 5 && $scope.villaOwnerName.length > 5) && $scope.villaNumber > 0)
         {
             console.log("ok");
         }
@@ -32,3 +32,29 @@ app.controller('villaSignInCtrl', ['$scope', function ($scope) {
     };
 
 }]);
+
+
+//Uploading images
+var ImageCtrl = [ '$scope', '$upload', function($scope, $upload) {
+    $scope.onFileSelect = function($files) {
+        //$files: an array of files selected, each file has name, size, and type.
+        for (var i = 0; i < $files.length; i++) {
+            if($files.type == "image")
+            var file = $files[i];
+            $scope.upload = $upload.upload({
+                url: 'server/upload/url', //upload.php script, node.js route, or servlet url
+                //method: 'POST' or 'PUT',
+                //headers: {'header-key': 'header-value'},
+                //withCredentials: true,
+                data: {myObj: $scope.myModelObj},
+                file: file
+            }).progress(function(evt) {
+                console.log('percent: ' + parseInt(100.0 * evt.loaded / evt.total));
+            }).success(function(data, status, headers, config) {
+                // file is uploaded successfully
+                console.log(data);
+            }
+            )
+        }}
+
+}];
